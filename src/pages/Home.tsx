bceb9049e1f987b5dcee5e0c46fb12e1773b1954
mulybirish <mulugata13@gmail.com>
2024-02-28 14:09:48 +0100

    fixed browse collection button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,46 +1,25 @@
-import { use } from "i18next";
-import { useState, useEffect } from "react";
-
+import React from "react";
 import { Container, Card, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Store } from "./Store";
 
-type ResponseType = {
-  data: string[];
-};
-
 export function Home() {
-  const [post, setPost] = useState([]);
-  const handle = async () => {};
-  // const count = counter(); // output : 4  // the value of the count is now available outside the function scope - clousere
-  useEffect(() => {
-    fetch("https://api.lyrics.ovh/v1/tool/the-pot")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setPost(data);
-      });
-  }, []);
-
-  console.log(post);
   return (
     <Container className="mt-5">
       <div className="row">
         <div className="col-md-6">
           <h1>Welcome to Our Music Gear Store</h1>
+
           <p className="lead">
             Explore our extensive collection of high-quality music gear, curated
             by professionals for musicians like you.
           </p>
-          <p>{post}</p>
+          <p>
+            Whether you're a beginner or a seasoned pro, we have the perfect
+            gear to elevate your sound.
+          </p>
           <Link to={"/store"}>
-            <Button
-              onClick={() => alert("hi")}
-              variant="primary"
-              href="#"
-              role="buttons"
-            >
+            <Button variant="primary" role="buttons">
               Browse Collection
             </Button>
           </Link>
